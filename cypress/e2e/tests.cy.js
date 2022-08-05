3034220b67ff15af98a1183cbaf8320cd6d03465
Mikko Hyyryläinen <mikko.e.hyyrylainen@gmail.com>
2022-08-05 09:39:18 +0300

    test if tests fail

--- cypress/e2e/tests.cy.js
+++ cypress/e2e/tests.cy.js
@@ -1,7 +1,13 @@
 describe('Pokedex', function() {
   it('front page can be opened', function() {
     cy.visit('http://localhost:5000')
-    cy.contains('ivysaur')
+    cy.contains('ivysaurz')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+  it('pokemon link can be opened', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('caterpie').click();
+    cy.contains('shield dust')
+    cy.contains('run away')
   })
 })
