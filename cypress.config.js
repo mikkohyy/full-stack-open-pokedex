fbffc45acade339fae576135e268f5b14420733d
Mikko Hyyryläinen <mikko.e.hyyrylainen@gmail.com>
2022-08-06 11:53:07 +0300

    change pageLoadTimeOut (wrong) to pageLoadTimeout

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    pageLoadTimeOut: 120000,
+    pageLoadTimeout: 120000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
