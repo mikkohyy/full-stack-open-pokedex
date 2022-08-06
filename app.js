08c1efe133f76ed6e5fb4b43799485e8143df445
Mikko Hyyryläinen <mikko.e.hyyrylainen@gmail.com>
2022-08-06 15:23:06 +0300

    add version route

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/health', (req, res) => {
   res.send('ok')
 })
 
+app.get('/version', (req, res) => {
+  res.send('0.0.1')
+})
+
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
